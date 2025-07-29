68d38b5312cefa0eb2606666359df7c9d9482990
AlwaysSangar <alwanrafa08@gmail.com>
2025-07-29 11:32:22 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("btnKirim").addEventListener("click",function () {
+    const output = document.getElementById("output");
+    output.textContent = "BERPROSES ITU LEBIH PENTING DARIPADA INSTAN";
+});
